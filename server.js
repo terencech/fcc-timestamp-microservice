1c8fa6fa9981b0cdab44964fd9c784234d82725e
terencech <terence000@gmail.com>
2021-12-12 16:55:42 -0800

    /api/ returns unix and utc of current time

--- server.js
+++ server.js
@@ -25,6 +25,13 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api", function (req, res) {
+  res.json({
+    unix: Date.now(),
+    utc: new Date(Date.now()).toUTCString()
+  })
+});
+
 app.get("/api/:date", function (req, res) {
   const isoDateRegex = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/
 
