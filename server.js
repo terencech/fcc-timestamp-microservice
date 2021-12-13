fa960e457173a38f1802cca5a78177743df66a76
terencech <terence000@gmail.com>
2021-12-12 16:53:51 -0800

    /api/:date returns unix and utc for iso and unix format param, error if invalid

--- server.js
+++ server.js
@@ -26,13 +26,26 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/api/:date", function (req, res) {
-  let date;
-  req.params.date.match(/\d{13}/) ? date = new Date(Number.parseInt(req.params.date)) : date = new Date(req.params.date);
+  const isoDateRegex = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/
 
-  res.json({
-    unix: date.getTime(),
-    utc: date.toUTCString()
-  });
+  let date = undefined;
+  if (req.params.date.match(/^\d{13}$/)) {
+    date = new Date(Number.parseInt(req.params.date))
+  }
+  else if (req.params.date.match(isoDateRegex)) {
+    date = new Date(req.params.date)
+  }
+
+  if (date) {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    })
+  } else {
+    res.json({
+      error: "Invalid Date"
+    })
+  }
 });
 
 // listen for requests :)
