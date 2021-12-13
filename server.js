712b5213bf0f2d62acd52b3edfa4b8ebb803ded2
terencech <terence000@gmail.com>
2021-12-12 17:19:05 -0800

    swapped iso date regex for Date.parse()

--- server.js
+++ server.js
@@ -33,13 +33,11 @@ app.get("/api", function (req, res) {
 });
 
 app.get("/api/:date", function (req, res) {
-  const isoDateRegex = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/
-
   let date = undefined;
   if (req.params.date.match(/^\d+$/)) {
     date = new Date(Number.parseInt(req.params.date))
   }
-  else if (req.params.date.match(isoDateRegex)) {
+  else if (Date.parse(req.params.date)) {
     date = new Date(req.params.date)
   }
 
